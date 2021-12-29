b8923412e44b4ae81abf2637615845492ab37b2f
yminyel <54792536+yminyel@users.noreply.github.com>
2021-12-29 13:21:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
 const myVideo = document.getElementById('myVideo')
 const container = document.getElementById('container')
-// const audio = document.getElementById('audio')
-
 
 
 var loader = document.getElementById('preloader');
     window.addEventListener("load", function(){
-    loader.style.display = "none"; 
 })
+
 myVideo.muted = 'false'; 
 myVideo.addEventListener('ended', vidEnded)
 
-const vids = ["part1.mp4","part2.mp4","part3.mp4","part4.mp4","part5.mp4","part6.mp4"]
+const vids = ["1.mp4","2.MP4","3.mp4","4.mp4"]
 let vidPlaying = 0;
- 
 
 function vidEnded(){
     container.addEventListener('click', checker, true)
@@ -26,9 +23,9 @@ function checker(){
 }
 
 function nextVideo(){
-    if(vidPlaying < vids.length){
+    if(vidPlaying< vids.length){
         vidPlaying++;
-    } 
+    }
     myVideo.src = "videos/" + vids[vidPlaying];
     vidStatus = true;
 }
