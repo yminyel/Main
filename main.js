2d236d3c9e272fd36e24a54d43113807488743f3
yminyel <54792536+yminyel@users.noreply.github.com>
2022-01-08 23:39:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var loader = document.getElementById('preloader');
 myVideo.muted = 'false'; 
 myVideo.addEventListener('ended', vidEnded)
 
-const vids = ["1.mp4","2.mp4","3.MP4"]
+const vids = ["1.mp4","2.mp4","3.mp4", "4.mp4","5.mp4"]
 let vidPlaying = 0;
 
 function vidEnded(){
